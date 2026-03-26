259edbbc73c825a8ee6998b87210db2b70dff91e
Sarunya <sarunya46mk@gmail.com>
2026-03-26 18:26:29 +0700

    chore(build): env error

--- src/constants/important.constant.ts
+++ src/constants/important.constant.ts
@@ -2,4 +2,4 @@ import { clientEnv } from "@/lib/env/env.client";
 
 export const BASE_URL = clientEnv.NEXT_PUBLIC_BASE_URL;
 export const LOGO_URL = `${BASE_URL}/logo.png`;
-export const GA_ID = clientEnv.NEXT_PUBLIC_GA_ID;
+// export const GA_ID = clientEnv.NEXT_PUBLIC_GA_ID;
